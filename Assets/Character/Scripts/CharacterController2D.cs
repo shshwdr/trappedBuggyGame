9396216435e027a43aeab7a23f7fa0dd096d209d
shshwdr <cs.vivian04@gmail.com>
2022-06-11 16:08:44 -0700

    fix jump

--- Assets/Character/Scripts/CharacterController2D.cs
+++ Assets/Character/Scripts/CharacterController2D.cs
@@ -73,7 +73,7 @@ public class CharacterController2D : MonoBehaviour
 		Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
 		for (int i = 0; i < colliders.Length; i++)
 		{
-			if (colliders[i].gameObject != gameObject)
+			if (colliders[i].gameObject != gameObject && colliders[i].transform.parent!=transform)
 			{
 				m_Grounded = true;
 				if (!wasGrounded)
